ee6dfa9b4ea87baccd0c9903afd031a9e2ffb59b
Raquel Blanco Morago <33464889+raqblanc@users.noreply.github.com>
2019-11-27 20:36:45 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import {fakeAuth as auth} from './auth'
 import LoginPage from './components/auth/LoginPage'
 import RegisterPage from './components/auth/RegisterPage'
 import Pictogram from './components/pictogram/Pictogram'
+import GroupsPage from './components/kidprofile/GroupsPage'
+import ViewKids from './components/kidprofile/ViewKids'
+import CreateGroup from './components/kidprofile/CreateGroup'
 
 function PrivateRoute({ children, ...rest}) {
 	return (
@@ -39,6 +42,15 @@ export default () => (
 			</Route>
 			<Route path="/signup">
 				<RegisterPage />
+			</Route>
+			<Route path="/groups">
+				<GroupsPage />
+			</Route>
+			<Route path="/kidspage">
+				<ViewKids />
+			</Route>
+			<Route path="/createGroup">
+				<CreateGroup />
 			</Route>
 			<PrivateRoute path="/">
 				<Pictogram />
