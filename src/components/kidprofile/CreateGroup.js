b96058fd010c333d113310b413088eac4e06e9e9
Raquel Blanco Morago <33464889+raqblanc@users.noreply.github.com>
2019-11-28 17:55:48 +0100

    Add files via upload

--- src/components/kidprofile/CreateGroup.js
+++ src/components/kidprofile/CreateGroup.js
@@ -1,50 +1,59 @@
 import React from 'react'
 import {
-	useHistory,
-	useLocation
+    useHistory,
+    useLocation
 } from 'react-router-dom'
 import { Jumbotron, Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 class CreateGroup extends React.Component {
     constructor(props) {
         super(props);
-        
+
+    }
+
+    createGroup() {
+
     }
     render() {
         return (
             <Container>
-                <Row>
-                    <Col md={5} className="mx-auto">
-                        <div id="first">
-                            <picture>
-                                <img alt="Logo Largo" width="100%" src="images/logolargo.png" />
-                            </picture>
-                            <div className="myform form">
-                                <div className="logo mb-3">
-                                    <Col md={12} className="text-center">
-                                        <h1>Crear un grupo</h1>
-                                    </Col>
-                                </div>
 
+                <Col md={5} className="mx-auto">
+                    <Row className="myrow">
 
-
-                                <Form onSubmit={this.handleSubmit}>
-                                    <FormGroup>
-                                        <Label>Nombre del grupo*</Label>
-                                        <Input
-                                            ref="nombre" required={true}
-                                            type="nombre" placeholder="Nombre" />
-                                    </FormGroup>
-
-                                    <Col md={12} className="text-center">
-                                        <Button type="submit" color="primary"
-                                            className="btn-block mybtn tx-tfm">Crear grupo</Button>
-                                    </Col>
-                                </Form>
+                        <picture>
+                            <img alt="Logo Largo" width="100%" src="images/logolargo.png" />
+                        </picture>
+                    </Row>
+                    <Row >
+                        <div className="myform form">
+                            <div className="logo mb-3">
+                                <Col md={12} className="text-center">
+                                    <h1>Crear un grupo</h1>
+                                </Col>
                             </div>
+
+
+
+                            <Form onSubmit={this.createGroup}>
+                                <FormGroup>
+                                    <Label>Nombre del grupo*</Label>
+                                    <Input
+                                        ref="nombre" required={true}
+                                        type="nombre" placeholder="Nombre" />
+                                </FormGroup>
+
+                                <Col md={12} className="text-center">
+                                    <Button type="submit" color="primary"
+                                        className="btn-block mybtn tx-tfm">Crear grupo</Button>
+                                </Col>
+                            </Form>
+
+
                         </div>
-                    </Col>
-                </Row>
+                    </Row>
+                </Col>
+
             </Container>
         );
     }
