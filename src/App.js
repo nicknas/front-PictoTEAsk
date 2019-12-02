0b868d8fde7803153d82d782a6b65fe7e9c905a9
Raquel Blanco Morago <33464889+raqblanc@users.noreply.github.com>
2019-12-02 23:41:37 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 	Switch,
 	Route,
 	Redirect,
+	withRouter
 } from "react-router-dom"
 
 import {fakeAuth as auth} from './auth'
@@ -37,38 +38,53 @@ function PrivateRoute({ children, ...rest}) {
 	);
 }
 
-export default () => (
-	<Router>
-		<Switch>
-			<Route path="/login">
-				<LoginPage />
-			</Route>
-			<Route path="/signup">
-				<RegisterPage />
-			</Route>
-			<Route path="/groupspage">
-				<GroupsPage />
-			</Route>
-			<Route path="/creategroup">
-				<CreateGroup />
-			</Route>
-			<Route path="/createkid">
-				<CreateKid />
-			</Route>
-			<Route path="/associatekid">
-				<AssociateKid />
-			</Route>
-			
-			<Route path="/stories">
-				<Story />
-			</Route>
-			<Route path="/stories/:storyName">
-				<StoryContent/>
-			</Route>
-			<PrivateRoute path="/">
-				<KidsPage/>
-			</PrivateRoute>
-		</Switch>
-	</Router>
-)
-
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {listGroups: [{ name: "Primer ciclo primaria", kids: [] },
+		{ name: "Segundo ciclo primaria", kids: [] }
+		]};
+		this.addNewGroup = this.addNewGroup.bind(this);
+	}
+	addNewGroup(nameGroup) {
+		const listGroups = this.state.listGroups;
+		listGroups.push({name: nameGroup, kids: []});
+		this.setState({listGroups: listGroups});
+	}
+	render() {
+		return (
+				<Router>
+					<Switch>
+						<Route path="/login">
+							<LoginPage />
+						</Route>
+						<Route path="/signup">
+							<RegisterPage />
+						</Route>
+						<Route path="/groupspage">
+							<GroupsPage listGroups={this.state.listGroups}/>
+						</Route>
+						<Route path="/creategroup">
+							<CreateGroup createGroup={this.addNewGroup} history={this.props.history}/>
+						</Route>
+						<Route path="/createkid">
+							<CreateKid />
+						</Route>
+						<Route path="/associatekid">
+							<AssociateKid />
+						</Route>
+						
+						<Route path="/stories">
+							<Story />
+						</Route>
+						<Route path="/stories/:storyName">
+							<StoryContent/>
+						</Route>
+						<PrivateRoute path="/">
+							<KidsPage/>
+						</PrivateRoute>
+					</Switch>
+				</Router>
+		);
+	}
+} export default App;
