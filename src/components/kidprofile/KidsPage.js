a8bd410faddeed8e7367ff8765004c9379e0c524
Rodri <39036536+rodrigolagartera@users.noreply.github.com>
2019-12-16 22:15:10 +0100

    PendingTutorias

--- src/components/kidprofile/KidsPage.js
+++ src/components/kidprofile/KidsPage.js
@@ -10,10 +10,14 @@ const enlace = 'https://pictoteask2.000webhostapp.com'
 class KidsPage extends React.Component {
     constructor(props) {
         super(props);
-				this.state = {
-					listKids: []
-				}
+        this.state = {
+            listKids: [],
+            listPending: []
+
+        }
         this.deleteKid = this.deleteKid.bind(this);
+        this.getPending = this.getPending.bind(this);
+        this.statusKid = this.statusKid.bind(this);
     }
 
     componentDidMount() {
@@ -31,16 +35,16 @@ class KidsPage extends React.Component {
             .then((response) => { if (response.ok) return response.json(); })
             .then(data => {
                 for (let i = 0; i < data.kids.length; i++) {
-                    if(data.kids[i].state == "ACCEPTED"){
-											listKids.push({
-												id: data.kids[i].id_kid,
-												name: data.kids[i].nick,
-												state: data.kids[i].state
-											});
+                    if (data.kids[i].state == "ACCEPTED") {
+                        listKids.push({
+                            id: data.kids[i].id_kid,
+                            name: data.kids[i].nick,
+                            state: data.kids[i].state
+                        });
                     }
                 }
 
-								this.setState({listKids})
+                this.setState({ listKids })
             });
     }
 
@@ -54,36 +58,90 @@ class KidsPage extends React.Component {
         formDataDisassociateKid.append('Nino', id);
         formDataDisassociateKid.append('Tutor', auth.token.id_tutor);
 
-        let {listKids} = this.state;
+        let { listKids } = this.state;
         let auxkids = listKids;
 
         listKids = listKids.filter(item => {
             return item.id != id;
         });
 
-				this.setState({listKids})
+        this.setState({ listKids })
 
         fetch(`${enlace}/delKid.php`, {
             method: 'POST',
             body: formDataDelKid
         }).then(response => response.json())
-				.then((response) => {
-						if (response.error) this.setState({listKids: auxkids});
-				});
+            .then((response) => {
+                if (response.error){
+                    fetch(`${enlace}/delTutoria.php`, {
+                        method: 'POST',
+                        body: formDataDisassociateKid
+                    }).then(response => response.json())
+                        .then((response) => {
+                            if (response.error){
+                                this.setState({ listKids: auxkids });
+                            }
+                        });
+                }
+                });
     }
 
+    statusKid(s,i){
+        let auth = new Auth();
+        let formDataPenKid = new FormData();
+        formDataPenKid.append('id_tutor', auth.token.id_tutor);
+        formDataPenKid.append('id_tutor_accion', this.state.listPending[i].id_tutor);
+        formDataPenKid.append('id_kid', this.state.listPending[i].id_kid);
+        formDataPenKid.append('accion', s);
 
+        fetch(`${enlace}/updateTutoria.php`, {
+            method: 'POST',
+            body: formDataPenKid
+        }).then((response) => { if (response.ok) return response.json(); })
+            .then((data) => {
+                if (!data.error) {
+                    
+                }
+                }
+            );
+    }
+    getPending() {
+        let auth = new Auth();
+        let listPending = [];
+        let formDataPenKid = new FormData();
+        formDataPenKid.append('id_tutor', auth.token.id_tutor);
+
+        fetch(`${enlace}/getPendingTutorias.php`, {
+            method: 'POST',
+            body: formDataPenKid
+        }).then((response) => { if (response.ok) return response.json(); })
+            .then((data) => {
+                if (!data.error) {
+                    for (let i = 0; i < data.turorias.length; i++) {
+                        listPending.push({
+                            id_tutor: data.turorias[i].id_tutor,
+                            nombre: data.turorias[i].nombre,
+                            id_kid: data.turorias[i].id_kid,
+                            nick: ""
+                        });
+                    }
+                    this.setState({ listPending });
+                }
+            });
+    }
     render() {
 
         return (
-					<ViewKids
-						from="/groupspage"
-						from2="/associatekid"
-						parent={this}
-						location={this.props.location}
-						history={this.props.history}
-						listKids={this.state.listKids}
-						deleteKid={this.deleteKid} />
+            <ViewKids
+                from="/groupspage"
+                from2="/associatekid"
+                parent={this}
+                location={this.props.location}
+                history={this.props.history}
+                listKids={this.state.listKids}
+                deleteKid={this.deleteKid}
+                getPending={this.getPending} 
+                statusKid = {this.statusKid}/>
         );
     }
 
--- src/components/kidprofile/ViewKids.js
+++ src/components/kidprofile/ViewKids.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {
-	Container,
-	Row,
-	Col,
-	Button,
-	Modal,
-	ModalHeader,
-	ModalBody,
-	ModalFooter
+    Container,
+    Row,
+    Col,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
 } from 'reactstrap';
 
 import './groups.css'
@@ -17,6 +17,7 @@ class ViewKids extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            pendingModalOpened: true,
             deleteModalOpened: false,
             addModalOpened: false,
             deleteModalOpened: false,
@@ -28,20 +29,23 @@ class ViewKids extends React.Component {
         this.closeDeleteModal = this.closeDeleteModal.bind(this);
         this.goGroups = this.goGroups.bind(this);
         this.goCreateKid = this.goCreateKid.bind(this);
+        this.closePendingModal = this.closePendingModal.bind(this);
+        this.openPendingModal = this.openPendingModal.bind(this);
+        this.statusKid = this.statusKid.bind(this);
     }
 
     goGroups(event) {
 
-			event.preventDefault();
-			let { from, history, location } = this.props;
+        event.preventDefault();
+        let { from, history, location } = this.props;
 
-			history.push({
-				pathname: from,
-				'state': {
-					'from': {'pathname': location.pathname },
-					'data': this.props.listKids
-				}
-			});
+        history.push({
+            pathname: from,
+            'state': {
+                'from': { 'pathname': location.pathname },
+                'data': this.props.listKids
+            }
+        });
 
     }
 
@@ -53,51 +57,90 @@ class ViewKids extends React.Component {
         history.push(from2);
     }
 
-	onClick(kid) {
-		let { history, location } = this.props;
+    onClick(kid) {
+        let { history, location } = this.props;
 
-		history.push({
-			pathname: '/calendar',
-			'state': {
-				'from': {'pathname': location.pathname },
-				'data': kid
-			}
-		});
+        history.push({
+            pathname: '/calendar',
+            'state': {
+                'from': { 'pathname': location.pathname },
+                'data': kid
+            }
+        });
 
-	}
+    }
 
     closeDeleteModal() {
-			this.setState({
-				kidToDelete: "",
-				idToDelete: "",
-				deleteModalOpened: false
-			});
+        this.setState({
+            kidToDelete: "",
+            idToDelete: "",
+            deleteModalOpened: false
+        });
     }
 
     deleteKid(event) {
         event.preventDefault();
 
         this.props.parent.deleteKid(this.state.idToDelete);
-				this.setState({
-					deleteModalOpened: false,
-					kidToDelete: ""
-				});
+        this.setState({
+            deleteModalOpened: false,
+            kidToDelete: ""
+        });
     }
 
     openDeleteModal(event, id) {
-      event.stopPropagation();
+        event.stopPropagation();
 
-      let kidName = event.currentTarget.parentNode.previousSibling.innerText;
+        let kidName = event.currentTarget.parentNode.previousSibling.innerText;
 
-			this.setState({
-				kidToDelete: kidName,
-				idToDelete: id,
-				deleteModalOpened: true
-			});
+        this.setState({
+            kidToDelete: kidName,
+            idToDelete: id,
+            deleteModalOpened: true
+        });
     }
+    statusKid(event, s, i) {
+        this.props.statusKid(s, i);
+    }
+    closePendingModal() {
+        this.setState({ pendingModalOpened: false });
+    }
+    openPendingModal() {
+        this.setState({ pendingModalOpened: true });
+    }
+    getPending() {
+        this.props.getPending();
+        let kids = this.props.parent.state.listKids;
+        let modals = "";
+        if (this.props.parent.state.listPending.length > 0) {
+            for (let i = 0; i < kids.length; i++)
+                for (let j = 0; j < this.props.parent.state.listPending.length; j++)
+                    if (kids[i].id == this.props.parent.state.listPending[j].id_kid)
+                        this.props.parent.state.listPending[j].nick = kids[i].name;
 
+
+            let listview = [];
+            for (let i = 0; i < this.props.parent.state.listPending.length; i++) {
+                listview.push(
+                    <div>
+                        <a>¿Está seguro de que quiere asociar el niño {this.props.parent.state.listPending[i].nick} a {this.props.parent.state.listPending[i].nombre} ?</a>
+                        <Button color="primary" onClick={(event) => this.statusKid(event, "ACCEPTED", i)}>Asociar</Button>
+                        <Button color="danger" onClick={(event) => this.statusKid(event, "REFUSED", i)}>Denegar</Button>
+                    </div>
+                );
+            }
+            modals = (<Modal
+                isOpen={this.state.pendingModalOpened}
+                toggle={this.closePendingModal}>
+                <ModalHeader toggle={this.openPendingModal}>Asociar niños</ModalHeader>
+                <ModalBody>{listview}</ModalBody>
+                <ModalFooter><Button color="secondary" onClick={this.closePendingModal}>Cancelar</Button></ModalFooter>
+            </Modal>);
+        }
+
+        return modals;
+    }
     render() {
-
         return (
             <Container>
                 <Row>
@@ -107,6 +150,7 @@ class ViewKids extends React.Component {
                                 <img alt="Logo Largo" width="100%" src="images/logolargo.png" />
                             </picture>
                         </Row>
+                        {this.getPending()}
                         <Container>
                             <Row className="mx-auto">
                                 <Col md={6} className="btncol">
@@ -124,21 +168,21 @@ class ViewKids extends React.Component {
 
                                     {this.props.listKids.map((row) =>
                                         <Row key={row.id} className="myrow btc-select">
-																					<Col onClick={() => this.onClick(row)} style={{'cursor': 'pointer'}} md={10} >
-																							<picture>
-																								<img
-																									src="images/defaultProfile.jpg"
-																									className="group-image" /> {row.name}
-																							</picture>
+                                            <Col onClick={() => this.onClick(row)} style={{ 'cursor': 'pointer' }} md={10} >
+                                                <picture>
+                                                    <img
+                                                        src="images/defaultProfile.jpg"
+                                                        className="group-image" /> {row.name}
+                                                </picture>
                                             </Col>
                                             <Col md={2} >
-                                                <picture style={{'cursor': 'pointer'}} onClick={(event) => this.openDeleteModal(event, row.id)}>
+                                                <picture style={{ 'cursor': 'pointer' }} onClick={(event) => this.openDeleteModal(event, row.id)}>
                                                     <img src="images/papelera.png" width="25px" />
                                                 </picture>
                                             </Col>
-																						<Modal
-																							isOpen={this.state.deleteModalOpened}
-																							toggle={this.closeDeleteModal}>
+                                            <Modal
+                                                isOpen={this.state.deleteModalOpened}
+                                                toggle={this.closeDeleteModal}>
                                                 <ModalHeader toggle={this.closeDeleteModal}>Desasociar niño</ModalHeader>
                                                 <ModalBody>¿Está seguro de que quiere desasociar el niño {this.state.kidToDelete}?</ModalBody>
                                                 <ModalFooter><Button id={row.id} key={row.id} color="danger" onClick={(event) => this.deleteKid(event)}>Desasociar</Button><Button color="secondary" onClick={this.closeDeleteModal}>Cancelar</Button></ModalFooter>
@@ -148,7 +192,7 @@ class ViewKids extends React.Component {
 
                                     <Row onClick={this.goCreateKid}>
                                         <Col>
-                                            <picture style={{'cursor': 'pointer'}}>
+                                            <picture style={{ 'cursor': 'pointer' }}>
                                                 <img src="../images/botonNew.svg" className="group-image" /> <font color="#3E8EDE">Añadir niño</font>
                                             </picture>
                                         </Col>
